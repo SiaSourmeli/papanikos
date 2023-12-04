9bff2432fcb0b529f12dbab30fdb84ed2d7b2b6f
SiaSourmeli <athanasia.sourmeli@gmail.com>
2023-12-04 13:54:43 +0200

    fix-theme-feat

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,20 @@ import About from 'src/components/about/about'
 import Services from 'src/components/services/services'
 import Work from 'src/components/work/work'
 import Contact from 'src/components/contact/contact'
-import Layout from 'src/components/layout'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Papanikos Plexiglas</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Layout>
-        <main>
-          <Hero />
-          <About />
-          <Services />
-          <Work />
-          <Contact />
-        </main>
-      </Layout> 
+      <Hero />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
     </div>
   )
 }
