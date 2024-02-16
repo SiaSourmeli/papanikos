e84e718b97de6e0388113a707ef50fe63b5576cd
Sia <athanasia.sourmeli@gmail.com>
2024-02-16 14:31:59 +0200

    replaced hero and aboutUs image

--- app/[lang]/components/hero/hero.js
+++ app/[lang]/components/hero/hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div id="" className={styles.heroContainer}>
       <img
-        src="https://ucarecdn.com/3a08ee59-83c9-4bd6-94e3-5744d746a5af/DSC_0150.jpg"
+        src="https://ucarecdn.com/4a1e7797-c900-4f12-b5bc-9814403a276c/HEADER_72dpi.jpg"
         className={styles.heroImage}
         alt="workshop"
       ></img>
